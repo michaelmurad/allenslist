a01dd4b254bcb39bdb29ff9bbc93f50f1a58e618
Michael Murad <neverthegreat@icloud.com>
2020-09-22 21:33:09 -0700

    updated private key to get it working

--- utilities/firebase/admin.ts
+++ utilities/firebase/admin.ts
@@ -3,7 +3,7 @@ import * as admin from 'firebase-admin';
 const firebaseAcount: admin.ServiceAccount = {
   projectId: process.env.FB_PROJECT_ID,
   clientEmail: process.env.FB_CLIENT_EMAIL,
-  privateKey: process.env.FB_PRIVATE_KEY,
+  privateKey: process.env.FB_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 };
 const services = {
   credential: admin.credential.cert(firebaseAcount),
